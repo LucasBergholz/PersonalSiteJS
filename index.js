1a11acd8cbaf286bbc5fa63a1c0e5e4f45c18eb2
LucasBergholz <lucas.bergholz@gmail.com>
2022-06-29 09:59:58 -0300

    Added Dark Mode

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function scrollIntoDiv(variable){
     document.getElementById(variable).scrollIntoView({behavior:"smooth"});
 } 
+
+function lightModeOption(){
+    var element = document.getElementById("cssMode")
+    var mode = element.getAttribute("href");
+
+        if(mode == "/Css/index.css"){
+            element.setAttribute("href",
+            "/Css/lightMode.css");
+        } else {
+            element.setAttribute("href",
+            "/Css/index.css");
+        }
+            
+}
